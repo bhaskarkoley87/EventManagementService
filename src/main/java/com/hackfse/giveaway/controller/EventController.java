eeb785d1bed166a665eb8cc1d59880eff579a175
bhaskarkoley87@gmail.com <bhaskar.koley1@metlife.com>
2019-03-19 19:19:58 +0530

    Committing the chnages for JUnit Testing.

--- src/main/java/com/hackfse/giveaway/controller/EventController.java
+++ src/main/java/com/hackfse/giveaway/controller/EventController.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +29,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
+import com.hackfse.giveaway.bean.ErrorResponse;
 import com.hackfse.giveaway.bean.EventBean;
+import com.hackfse.giveaway.exception.ReportException;
 import com.hackfse.giveaway.services.EventService;
 import com.hackfse.giveaway.services.FileUploadService;
 
@@ -92,4 +96,11 @@ public class EventController {
 		return IOUtils.toByteArray(in);
 	}
 
+	@ExceptionHandler(ReportException.class)
+	public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+		ErrorResponse error = new ErrorResponse();
+		error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
+	}
 }
